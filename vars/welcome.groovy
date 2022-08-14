ecc4d8f46acd012fb9578ccb9317e77fd0e3ae0a
Tvssandeep <91420763+Tvssandeep@users.noreply.github.com>
2022-08-14 13:51:09 +0530

    Update welcome.groovy

--- vars/welcome.groovy
+++ vars/welcome.groovy
@@ -1,3 +1,7 @@
-def call(name){
-echo " Hey ${name} welcome to Sharedlibs"
+def call(){
+sshagent(['tomcat-dev']) {
+                   sh "scp -o StrictHostKeyChecking=no target/myweb.war ec2-user@172.31.3.59:/opt/tomcat8/webapps/"
+                   sh "ssh ec2-user@172.31.3.59 /opt/tomcat8/bin/shutdown.sh"
+                   sh "ssh ec2-user@172.31.3.59 /opt/tomcat8/bin/startup.sh"
+              }
 }
