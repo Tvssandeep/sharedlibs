027c773065cc29eca83c1bf2321d61b766638ab2
Tvssandeep <91420763+Tvssandeep@users.noreply.github.com>
2022-08-14 13:57:04 +0530

    Update welcome.groovy

--- vars/welcome.groovy
+++ vars/welcome.groovy
@@ -1,6 +1,6 @@
 def call(){
 sshagent(['tomcat-dev']) {
-                   sh "scp -o StrictHostKeyChecking=no target/myweb.war ec2-user@172.31.3.59:/opt/tomcat8/webapps/"
+                   sh "scp -o StrictHostKeyChecking=no target/myweb*.war ec2-user@172.31.3.59:/opt/tomcat8/webapps/"
                    sh "ssh ec2-user@172.31.3.59 /opt/tomcat8/bin/shutdown.sh"
                    sh "ssh ec2-user@172.31.3.59 /opt/tomcat8/bin/startup.sh"
               }
